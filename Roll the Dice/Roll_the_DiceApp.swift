94cf46fb31f4a6d34b73b65af2e8b486c43f3c48
Ryan Lim <rlim@bsd220.org>
2021-11-08 18:30:51 -0600

    Initial Commit

---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Student on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
